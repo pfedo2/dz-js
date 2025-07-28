47460b1b59c592da6063ccf4429a8603722385dc
pfedo2 <black.cat45562@gmail.com>
2025-07-28 12:56:29 -0400

    hh

---
+++ dz3/dz-3.js
@@ -0,0 +1,15 @@
+let result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+let email = "example@gmail.com";
+let containsAt = email.includes("@");
+let emailLength = email.length;
+console.log("Містить @:", containsAt);
+console.log("Кількість символів:", emailLength);
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = word1 + " " + word2 + " " + word3 + " Viktor";
+console.log(fullName);
